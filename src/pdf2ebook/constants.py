aa144ca603afdca446fbbd818b1dcf7fb7ed9c3f
Robert Lucey <robert@glimpse.ie>
2022-10-29 17:46:35 +0100

    Change log loc on test

--- src/pdf2ebook/constants.py
+++ src/pdf2ebook/constants.py
@@ -1,1 +1,1 @@
-LOG_LOCATION = "/var/log/pdf2ebook/pdf2ebook.log"
+LOG_LOCATION = "/var/log/pdf2ebook/pdf2ebook.log" if os.getenv("TEST_ENV", "False") == "False" else "/tmp/log/pdf2ebook/pdf2ebook.log"
