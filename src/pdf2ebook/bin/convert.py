068d525579056836cac39177bae2ffe6fd870682
Robert Lucey <robert@glimpse.ie>
2022-10-29 18:21:53 +0100

    Ability to force html / text

--- src/pdf2ebook/bin/convert.py
+++ src/pdf2ebook/bin/convert.py
@@ -8,9 +8,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--in", type=str, dest="in_file", required=True)
     parser.add_argument("--out", type=str, dest="out_file", required=True)
+    parser.add_argument(
+        "--force-html",
+        action="store_true",
+        dest="force_html",
+    )
+    parser.add_argument(
+        "--force-text",
+        action="store_true",
+        dest="force_text",
+    )
     args = parser.parse_args()
 
-    pdf = PDF(path=args.in_file)
+    pdf = PDF(path=args.in_file, use_html=args.force_html, use_text=args.force_text)
     pdf.load()
     pdf.to_epub(path=args.out_file)
 
