380030caaf9ccde7bcdf5dadbf17c41996317872
Robert Lucey <robert@glimpse.ie>
2022-10-30 13:16:52 +0000

    Add room with a view
    Will extend these tests at some point

--- test/test_pdf.py
+++ test/test_pdf.py
@@ -32,7 +32,6 @@ class AlicePDFTest(TestCase):
                 '1'
             )
         )
-        pdf.pages._data[0].text_content[21:35]
 
         self.assertTrue(
             'in Wonderland' in pdf.pages._data[0].text_content
@@ -49,6 +48,7 @@ class AlicePDFTest(TestCase):
         self.assertTrue(
             os.path.exists('/tmp/test_alice.epub')
         )
+
 
 class VortexPDFTest(TestCase):
 
@@ -77,7 +77,6 @@ class VortexPDFTest(TestCase):
                 'INTRODUCING'
             )
         )
-        pdf.pages._data[0].text_content[21:35]
 
     def test_to_epub(self):
         pdf = PDF(path='test/resources/vortex.pdf')
@@ -87,3 +86,41 @@ class VortexPDFTest(TestCase):
         except:
             pass
         pdf.to_epub('/tmp/test_vortex.epub')
+
+
+class RoomWithAViewPDFTest(TestCase):
+
+    def test_to_text(self):
+        pass
+
+    def test_to_html(self):
+        pdf = PDF(path='test/resources/room_with_a_view.pdf')
+        pdf.load()
+        self.assertEquals(len(pdf.pages), 151)
+
+    def test_pages_context(self):
+        pdf = PDF(path='test/resources/room_with_a_view.pdf')
+        pdf.load()
+        self.assertEquals(
+            pdf.pages._data[10].next_page.idx,
+            pdf.pages._data[11].idx,
+        )
+
+    def test_pages_content(self):
+        pdf = PDF(path='test/resources/room_with_a_view.pdf')
+        pdf.load()
+
+        self.assertTrue(
+            pdf.pages._data[0].text_content.startswith(
+                'A ROOM'
+            )
+        )
+
+    def test_to_epub(self):
+        pdf = PDF(path='test/resources/room_with_a_view.pdf')
+        pdf.load()
+        try:
+            os.remove('/tmp/test_room_with_a_view.epub')
+        except:
+            pass
+        pdf.to_epub('/tmp/test_room_with_a_view.epub')
