b532334aec1b24c19e558a2a029db7c8849b7d00
Robert Lucey <robert@glimpse.ie>
2022-10-30 14:15:21 +0000

    Clean pdf tests

---
+++ test/test_pdf_alice.py
@@ -0,0 +1,57 @@
+import os
+
+from unittest import TestCase
+
+from pdf2ebook.pdf import PDF
+
+
+class AlicePDFTest(TestCase):
+
+    PDF_PATH = 'test/resources/alice.pdf'
+    EPUB_NAME = 'test_alice.epub'
+
+    def test_to_html(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        self.assertEquals(len(pdf.pages), 21)
+
+    def test_pages_content(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+
+        self.assertTrue(
+            pdf.pages._data[0].text_content.startswith(
+                '1'
+            )
+        )
+
+        self.assertTrue(
+            'in Wonderland' in pdf.pages._data[0].text_content
+        )
+
+    def test_pages_context(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        for i in range(len(pdf.pages)):
+            if i == len(pdf.pages) - 1:
+                self.assertEquals(
+                    pdf.pages._data[i].next_page,
+                    None,
+                )
+            else:
+                self.assertEquals(
+                    pdf.pages._data[i].next_page.idx,
+                    pdf.pages._data[i + 1].idx,
+                )
+
+    def test_to_epub(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        try:
+            os.remove(f'/tmp/{self.EPUB_NAME}')
+        except:
+            pass
+        pdf.to_epub(f'/tmp/{self.EPUB_NAME}')
+        self.assertTrue(
+            os.path.exists(f'/tmp/{self.EPUB_NAME}')
+        )
---
+++ test/test_pdf_room_with_a_view.py
@@ -0,0 +1,53 @@
+import os
+
+from unittest import TestCase
+
+from pdf2ebook.pdf import PDF
+
+
+class RoomWithAViewPDFTest(TestCase):
+
+    PDF_PATH = 'test/resources/room_with_a_view.pdf'
+    EPUB_NAME = 'test_room_with_a_view.epub'
+
+    def test_to_html(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        self.assertEquals(len(pdf.pages), 151)
+
+    def test_pages_content(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+
+        self.assertTrue(
+            pdf.pages._data[0].text_content.startswith(
+                'A ROOM'
+            )
+        )
+
+    def test_pages_context(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        for i in range(len(pdf.pages)):
+            if i == len(pdf.pages) - 1:
+                self.assertEquals(
+                    pdf.pages._data[i].next_page,
+                    None,
+                )
+            else:
+                self.assertEquals(
+                    pdf.pages._data[i].next_page.idx,
+                    pdf.pages._data[i + 1].idx,
+                )
+
+    def test_to_epub(self):
+        pdf = PDF(path=self.PDF_PATH)
+        pdf.load()
+        try:
+            os.remove(f'/tmp/{self.EPUB_NAME}')
+        except:
+            pass
+        pdf.to_epub(f'/tmp/{self.EPUB_NAME}')
+        self.assertTrue(
+            os.path.exists(f'/tmp/{self.EPUB_NAME}')
+        )
