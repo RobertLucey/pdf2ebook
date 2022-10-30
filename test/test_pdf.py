a77522f9a7f9724032cc99956b54c8331f7773b5
Robert Lucey <robert@glimpse.ie>
2022-10-30 10:42:53 +0000

    Add more test resources

--- test/test_pdf.py
+++ test/test_pdf.py
@@ -1,9 +1,11 @@
+import os
+
 from unittest import TestCase
 
 from pdf2ebook.pdf import PDF
 
 
-class PDFTest(TestCase):
+class AlicePDFTest(TestCase):
 
     def test_to_text(self):
         pass
@@ -39,4 +41,49 @@ class PDFTest(TestCase):
     def test_to_epub(self):
         pdf = PDF(path='test/resources/alice.pdf')
         pdf.load()
-        pdf.to_epub('/tmp/test.epub')
+        try:
+            os.remove('/tmp/test_alice.epub')
+        except:
+            pass
+        pdf.to_epub('/tmp/test_alice.epub')
+        self.assertTrue(
+            os.path.exists('/tmp/test_alice.epub')
+        )
+
+class VortexPDFTest(TestCase):
+
+    def test_to_text(self):
+        pass
+
+    def test_to_html(self):
+        pdf = PDF(path='test/resources/vortex.pdf')
+        pdf.load()
+        self.assertEquals(len(pdf.pages), 16)
+
+    def test_pages_context(self):
+        pdf = PDF(path='test/resources/vortex.pdf')
+        pdf.load()
+        self.assertEquals(
+            pdf.pages._data[10].next_page.idx,
+            pdf.pages._data[11].idx,
+        )
+
+    def test_pages_content(self):
+        pdf = PDF(path='test/resources/vortex.pdf')
+        pdf.load()
+
+        self.assertTrue(
+            pdf.pages._data[0].text_content.startswith(
+                'INTRODUCING'
+            )
+        )
+        pdf.pages._data[0].text_content[21:35]
+
+    def test_to_epub(self):
+        pdf = PDF(path='test/resources/vortex.pdf')
+        pdf.load()
+        try:
+            os.remove('/tmp/test_vortex.epub')
+        except:
+            pass
+        pdf.to_epub('/tmp/test_vortex.epub')
